e6967ef8df291829e3a64e2ced69506e2cceb3a3
LNemesis16 <andreiafglage@gmail.com>
2025-07-22 22:43:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print(f"Olá, {nome}! Prazer em te conhecer.")
 
 # Pergunta a idade do utilizador
 idade: int = int(input("Quantos anos você tem? "))
+
+# Resposta personalizada com base na idade
+if idade < 12:
+    print("Uau! Você é bem jovem para estar aprendendo Python, que incrível!")
+elif 12 <= idade <= 18:
+    print("Legal! Aprender Python na sua idade é uma ótima escolha!")
+elif 19 <= idade <= 40:
+    print("Muito bem! Python é excelente para qualquer área profissional.")
+else:
+    print("Nunca é tarde para aprender algo novo, parabéns!")
