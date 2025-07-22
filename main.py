c25f7d26ad09cf93fe52215b910a3af4bee28898
LNemesis16 <andreiafglage@gmail.com>
2025-07-22 22:42:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print("Tudo bem com vocês?")
 # Solicita o nome do utilizador
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}! Prazer em te conhecer.")
+
+# Pergunta a idade do utilizador
+idade: int = int(input("Quantos anos você tem? "))
