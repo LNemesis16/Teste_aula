e23eda111aadc3351e3240011c91811ee1f6a3ce
LNemesis16 <andreiafglage@gmail.com>
2025-07-22 22:41:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Saudação inicial
 print("Olá turma do Python!")
 print("Tudo bem com vocês?")
+
+# Solicita o nome do utilizador
+nome: str = input("Qual é o seu nome? ")
+print(f"Olá, {nome}! Prazer em te conhecer.")
